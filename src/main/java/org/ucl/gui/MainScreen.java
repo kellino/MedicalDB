766ff5df07cff4ea5aaffb593cfdbeb96bb36809
David Kelly <thedavidkelly@hotmail.com>
2015-11-28 09:44:52 +0000

    more layout improvements

--- src/main/java/org/ucl/gui/MainScreen.java
+++ src/main/java/org/ucl/gui/MainScreen.java
@@ -4,24 +4,32 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.*;
+import javax.print.URIException;
 import javax.swing.*;
 import org.ucl.medicaldb.Database;
 
 /**
  * Creates the main screen gui, initializing the various members, such as the
- * menu bar and tabbed screen
+ * menu bar and tabbed screen. The screen is split horizontally into two main areas:
+ * the top area contains the main personal data of the loaded patient. This area is not
+ * editable. The lower half of the screen features the editing functions (on the left) and
+ * a tabbed pane with the specific medical condition information
  */
 @SuppressWarnings("serial")
 public class MainScreen extends JPanel {
-    protected static String[] currentIds = Database.idNumbers.stream().toArray(String[]::new); /* this code snippet was inspired by a 
-	                                                                                      * stackoverflow question */
-    protected static final String[][] dateFormat = { Database.days, Database.months, Database.years };
-    protected static final String[][] patientData = { currentIds, {"-", "Mr", "Mrs", "Ms", "Dr" }, {"-", "Male", "Female" } };
-    protected static final String[] fields = new String[] { "Patient ID", "Title", "Sex", "Last Name", "First Name(s)",
+	protected static String[] currentIds = Database.idNumbers.stream().toArray(String[]::new); /*
+										                    * this code snippet was inspired by a
+										                    * stackoverflow question
+										                    */
+	protected static final String[][] dateFormat = { Database.days, Database.months, Database.years };
+	protected static final String[][] patientData = { currentIds, { "-", "Mr", "Mrs", "Ms", "Dr" }, { "-", "Male", "Female" } };
+	protected static final String[] fields = new String[] { "Patient ID", "Title", "Sex", "Last Name", "First Name(s)",
 			"Date of Birth", "dd", "mm", "YY", "Condition(s)", "Address", "Next Appointment", "Comments" };
-    private static final Color LIGHT_BLUE = new Color(102, 178, 255, 240);
-    //private static final int WIDTH = GUI.WIDTH;
-    // private static final int HEIGHT = GUI.HEIGHT;
+	private static final Color LIGHT_BLUE = new Color(102, 178, 255, 220);
 
 	/** constructor for the main screen. */
 	public MainScreen() {
@@ -35,7 +43,9 @@ public class MainScreen extends JPanel {
 		c.gridx = 0;
 		c.gridy = 1;
 		c.weightx = 1.0;
+
 		JMenuBar menuBar = createMenu();
+
 		add(menuBar, c);
 
 		/* patient data panel, including patient details and profile photo */
@@ -43,7 +53,9 @@ public class MainScreen extends JPanel {
 		c.anchor = GridBagConstraints.NORTHWEST;
 		c.gridx = 0;
 		c.gridy = 5;
+
 		JPanel patientData = createPatientDataArea();
+
 		add(patientData, c);
 
 		/*
@@ -55,14 +67,14 @@ public class MainScreen extends JPanel {
 		c.gridx = 0;
 		c.gridy = 6;
 		c.weighty = 1.0;
+
 		JSplitPane medicalHistoryPanel = medicalHistoryPanel();
+
 		add(medicalHistoryPanel, c);
 	}
 
-	/**
-	 * overriding the paintComponent(Graphics g) method allows us to set an
-	 * image as the background for our JPanel
-	 */
+	/** overriding the paintComponent(Graphics g) method allows us to set an
+	 * image as the background for our JPanel */
 	@Override
 	protected void paintComponent(Graphics g) {
 		g.drawImage(LoginScreen.bgimage, 0, 0, null);
@@ -73,10 +85,12 @@ public class MainScreen extends JPanel {
 		JMenuBar mb = new JMenuBar();
 		MenuListener l = new MenuListener();
 		JMenuItem menuItem;
+
 		mb.setBounds(0, 0, 1200, 30);
 		mb.setBorder(BorderFactory.createEtchedBorder(1));
 
 		JMenu mnFile = new JMenu("File");
+
 		mnFile.setMnemonic(KeyEvent.VK_F);
 		/* save */
 		menuItem = mnFile.add(new JMenuItem("Save", 's'));
@@ -102,6 +116,7 @@ public class MainScreen extends JPanel {
 		mb.add(mnFile);
 
 		JMenu mnEdit = new JMenu("Edit");
+
 		mnEdit.setMnemonic(KeyEvent.VK_E);
 
 		/* menu items for the Edit menu */
@@ -116,10 +131,12 @@ public class MainScreen extends JPanel {
 		mb.add(mnEdit);
 
 		JMenu mnAbout = new JMenu("About");
+
 		mnAbout.setMnemonic(KeyEvent.VK_A);
 		mb.add(mnAbout);
 
 		JMenu mnHelp = new JMenu("Help");
+
 		mnHelp.setMnemonic(KeyEvent.VK_H);
 		mb.add(mnHelp);
 
@@ -136,6 +153,7 @@ public class MainScreen extends JPanel {
 	/** popup confirmation dialog for user-driven decisions */
 	private int confirmationDialog(String message, String title) {
 		int reply = JOptionPane.NO_OPTION;
+
 		try {
 			reply = JOptionPane.showConfirmDialog(null, message, title, JOptionPane.YES_NO_OPTION);
 		} catch (HeadlessException he) {
@@ -144,83 +162,87 @@ public class MainScreen extends JPanel {
 		return reply;
 	}
 
+	/** the upper half of the MainScreen gui. Initialized with a for loop containing
+	 * a switch statement */
 	private JPanel createPatientDataArea() {
+	        // we should not use null as a layout
 		JPanel area = new JPanel(null);
+
 		area.setPreferredSize(new Dimension(1200, 300));
 		area.setBackground(LIGHT_BLUE);
 		area.setBorder(BorderFactory.createEtchedBorder(1));
 
 		JLabel[] titleContainers = new JLabel[fields.length];
 		for (int i = 0; i < fields.length; i++) {
-			//int y_shift = (i - 2) * 40;
+			// int y_shift = (i - 2) * 40;
 			titleContainers[i] = new JLabel();
 			switch (i) {
 			case 0: /* patient id */
-			    titleContainers[i].setBounds(50, 40, 150, 30);
-			    titleContainers[i].setText(fields[i]);
-			    break;
+				titleContainers[i].setBounds(50, 40, 150, 30);
+				titleContainers[i].setText(fields[i]);
+				break;
 			case 1: /* title */
-			    titleContainers[i].setBounds(250, 40, 100, 30);
-			    titleContainers[i].setText(fields[i]);
-			    break;
+				titleContainers[i].setBounds(250, 40, 100, 30);
+				titleContainers[i].setText(fields[i]);
+				break;
 			case 2: /* sex */
-			    titleContainers[i].setBounds(340, 40, 100, 30);
-			    titleContainers[i].setText(fields[i]);
-			    break;
-                        case 3: /* last name */ 
-			    titleContainers[i].setBounds(50, 80, 100, 30);
-			    titleContainers[i].setText(fields[i]);
-			    break;
-                        case 4: /* first name */
-			    titleContainers[i].setBounds(50, 120, 100, 30);
-			    titleContainers[i].setText(fields[i]);
-			    break;
-                        case 5: /* first name */
-			    titleContainers[i].setBounds(50, 160, 100, 30);
-			    titleContainers[i].setText(fields[i]);
-			    break;
-                        case 10: /* address */
-			    titleContainers[i].setBounds(50, 200, 100, 30);
-			    titleContainers[i].setText(fields[i]);
-			    break;
+				titleContainers[i].setBounds(340, 40, 100, 30);
+				titleContainers[i].setText(fields[i]);
+				break;
+			case 3: /* last name */
+				titleContainers[i].setBounds(50, 80, 100, 30);
+				titleContainers[i].setText(fields[i]);
+				break;
+			case 4: /* first name */
+				titleContainers[i].setBounds(50, 120, 100, 30);
+				titleContainers[i].setText(fields[i]);
+				break;
+			case 5: /* first name */
+				titleContainers[i].setBounds(50, 160, 100, 30);
+				titleContainers[i].setText(fields[i]);
+				break;
+			case 10: /* address */
+				titleContainers[i].setBounds(50, 200, 100, 30);
+				titleContainers[i].setText(fields[i]);
+				break;
 			default:
-			    break;
+				break;
 			}
 			area.add(titleContainers[i]);
 		}
-
 		JTextField[] inputFields = new JTextField[fields.length];
 		for (int i = 0; i < fields.length; i++) {
-		    inputFields[i] = new JTextField();
-		    switch (i) {
-                        case 0: /* patient id */
-			    inputFields[i].setBounds(150, 40, 80, 30);
-			    break;
-                        case 1: /* title */
-			    inputFields[i].setBounds(280, 40, 50, 30);
-			    break;
-                        case 2: /* sex */
-			    inputFields[i].setBounds(370, 40, 80, 30);
-			    break;
-                        case 3: /* last name */
-			    inputFields[i].setBounds(150, 80, 150, 30);
-			    break;
-                        case 4: /* first name */
-			    inputFields[i].setBounds(150, 120, 250, 30);
-			    break;
-                        case 5: /* date of birth */
-			    inputFields[i].setBounds(150, 160, 150, 30);
-			    break;
-                        case 10: /* address */
-			    inputFields[i].setBounds(150, 200, 450, 30);
-			    break;
-                        default:
-                            break;
-                    }
-		    area.add(inputFields[i]);
+			inputFields[i] = new JTextField();
+			switch (i) {
+			case 0: /* patient id */
+				inputFields[i].setBounds(150, 40, 80, 30);
+				break;
+			case 1: /* title */
+				inputFields[i].setBounds(280, 40, 50, 30);
+				break;
+			case 2: /* sex */
+				inputFields[i].setBounds(370, 40, 80, 30);
+				break;
+			case 3: /* last name */
+				inputFields[i].setBounds(150, 80, 150, 30);
+				break;
+			case 4: /* first name */
+				inputFields[i].setBounds(150, 120, 250, 30);
+				break;
+			case 5: /* date of birth */
+				inputFields[i].setBounds(150, 160, 150, 30);
+				break;
+			case 10: /* address */
+				inputFields[i].setBounds(150, 200, 450, 30);
+				break;
+			default:
+				break;
+			}
+			area.add(inputFields[i]);
 		}
 
 		JLabel picture = new JLabel();
+
 		picture.setIcon(new ImageIcon("/home/david/Programming/Java/medicaldb/res/ab100.png"));
 		picture.setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, Color.BLUE));
 		picture.setBounds(900, 40, 200, 200);
@@ -229,56 +251,11 @@ public class MainScreen extends JPanel {
 		return area;
 	}
 
+	/** bottom split of the MainScreen gui. A JSplitPane */
 	private JSplitPane medicalHistoryPanel() {
 		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		JPanel databaseChanger = createDatabaseChanger();
 		JTabbedPane medicalDataPane = tabbedMedicalDataPane();
-		JPanel databaseChanger = new JPanel();
-                databaseChanger.setPreferredSize(new Dimension(300, 400));
-                databaseChanger.setBackground(new Color(100, 100, 100, 100));
-                databaseChanger.setLayout(new BoxLayout(databaseChanger, BoxLayout.X_AXIS));
-
-
-		JButton adder = new JButton();
-		adder.setText("Add patient");
-		adder.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				DatabaseEditor pa = new DatabaseEditor();
-				int result = JOptionPane.showConfirmDialog(null, pa, "Add Patient", JOptionPane.OK_CANCEL_OPTION,
-						JOptionPane.PLAIN_MESSAGE);
-				if (result == JOptionPane.OK_OPTION) {
-					System.out.println("Patient added");
-				} else {
-					System.out.println("Patient adding cancelled");
-				}
-			}
-		});
-		databaseChanger.add(adder);
-
-		JButton editor = new JButton();
-		editor.setText("Edit patient");
-		editor.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				DatabaseEditor pa = new DatabaseEditor("ab100");
-				int result = JOptionPane.showConfirmDialog(null, pa, "Edit Patient", JOptionPane.OK_CANCEL_OPTION,
-						JOptionPane.PLAIN_MESSAGE);
-				if (result == JOptionPane.OK_OPTION) {
-					System.out.println("Patient edited");
-				} else {
-					System.out.println("Patient editing cancelled");
-				}
-			}
-		});
-		databaseChanger.add(editor);
-
-		JButton search = new JButton();
-		search.setText("Search");
-		search.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			    // to be implemented
-			    System.out.println("Patient search");
-			}
-		});
-		databaseChanger.add(search);
 
 		splitPane.setLeftComponent(databaseChanger);
 		splitPane.setRightComponent(medicalDataPane);
@@ -289,15 +266,72 @@ public class MainScreen extends JPanel {
 		return splitPane;
 	}
 
+        /** a JPanel with three buttons, one for editing a patient, one for adding a patient,
+         * and one for launching the search function */
+        private JPanel createDatabaseChanger() {
+		JPanel databaseChanger = new JPanel();
+
+		databaseChanger.setPreferredSize(new Dimension(300, 400));
+		databaseChanger.setBackground(new Color(100, 100, 100, 100));
+		databaseChanger.setLayout(new BorderLayout());
+
+		JButton adder = new JButton();
+
+		adder.setText("Add patient");
+		adder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DatabaseEditor pa = new DatabaseEditor();
+				int result = JOptionPane.showConfirmDialog(null, pa, "Add Patient", JOptionPane.OK_CANCEL_OPTION,
+						JOptionPane.PLAIN_MESSAGE);
+				if (result == JOptionPane.OK_OPTION)
+					System.out.println("Patient added");
+				else
+					System.out.println("Patient adding cancelled");
+			}
+		});
+		databaseChanger.add(adder);
+
+		JButton editor = new JButton();
+
+		editor.setText("Edit patient");
+		editor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DatabaseEditor pa = new DatabaseEditor("ab100");
+				int result = JOptionPane.showConfirmDialog(null, pa, "Edit Patient", JOptionPane.OK_CANCEL_OPTION,
+						JOptionPane.PLAIN_MESSAGE);
+				if (result == JOptionPane.OK_OPTION)
+					System.out.println("Patient edited");
+				else
+					System.out.println("Patient editing cancelled");
+			}
+		});
+		databaseChanger.add(editor);
+
+		JButton search = new JButton();
+
+		search.setText("Search");
+		search.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// to be implemented
+				System.out.println("Patient search");
+			}
+		});
+		databaseChanger.add(search);
+
+		return databaseChanger;
+        }
+
+	/** a JTabbedPane which houses the medical history pane (GridBagLayout) and the 
+	 * photo pane (GridLayout) */
 	private JTabbedPane tabbedMedicalDataPane() {
 		JTabbedPane medicalDataPane = new JTabbedPane();
+
 		medicalDataPane.setMinimumSize(new Dimension(500, 400));
 		medicalDataPane.setPreferredSize(new Dimension(800, 400));
 
 		JPanel medicalHistory = createMedicalHistoryPane();
-
-		JPanel photos = new JPanel();
-		photos.setBackground(Color.BLUE);
+		
+		JPanel photos = createPhotoPane();
 
 		medicalDataPane.add("Medical History", medicalHistory);
 		medicalDataPane.add("Photographs", photos);
@@ -307,19 +341,59 @@ public class MainScreen extends JPanel {
 
 	private JPanel createMedicalHistoryPane() {
 		JPanel medicalHistory = new JPanel();
-		medicalHistory.setBackground(Color.DARK_GRAY);
-		medicalHistory.setLayout(new FlowLayout());
-		medicalHistory.setBorder(BorderFactory.createBevelBorder(1));
 
+		medicalHistory.setBackground(Color.DARK_GRAY);
+		medicalHistory.setBorder(BorderFactory.createBevelBorder(1));
+		medicalHistory.setLayout(new GridBagLayout());
+                GridBagConstraints c = new GridBagConstraints();
+
+                c.fill = GridBagConstraints.HORIZONTAL;
+                c.gridx = 0;
+                c.gridy = 0;
 		JLabel condition = new JLabel();
 		condition.setText("Medical Condition(s)");
-		condition.setBackground(Color.LIGHT_GRAY);
 		condition.setForeground(Color.WHITE);
-		medicalHistory.add(condition);
+		condition.setPreferredSize(new Dimension(100, 30));
+		medicalHistory.add(condition, c);
 
+		c.weightx = 0.5;
 		JTextField conditionField = new JTextField();
-		conditionField.add(condition);
+		conditionField.setPreferredSize(new Dimension(400, 30));
+                medicalHistory.add(conditionField);
 
+                c.fill = GridBagConstraints.HORIZONTAL;
+                c.gridy = 1;
+                JLabel link = new JLabel();
+                link.setText("Click for more information on " + "condition");
+                link.setForeground(Color.WHITE);
+                link.setPreferredSize(new Dimension(150, 30));
+                link.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                link.addMouseListener(new MouseAdapter() {
+                    public void mouseClicked(MouseEvent e) {
+                        if (e.getClickCount() > 0) {
+                            if (Desktop.isDesktopSupported()) {
+                                Desktop desktop = Desktop.getDesktop();
+                                try {
+                                    URI uri = new URI("http://www.bbc.co.uk");
+                                    desktop.browse(uri);
+                                } catch (IOException ioe) {
+                                    ioe.printStackTrace();
+                                } catch (URISyntaxException ue) {
+                                    ue.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                });
+		medicalHistory.add(link, c);
 		return medicalHistory;
+
+	}
+
+	private JPanel createPhotoPane() {
+		JPanel photos = new JPanel();
+		photos.setBackground(Color.BLUE);
+
+		return photos;
 	}
 }
