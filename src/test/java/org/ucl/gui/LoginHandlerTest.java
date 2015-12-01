9b0f56825a4d2c7f68d210215e8b26603a3d025c
David Kelly <thedavidkelly@hotmail.com>
2015-12-01 22:01:06 +0000

    small changes to maven dependencies

---
+++ src/test/java/org/ucl/gui/LoginHandlerTest.java
@@ -0,0 +1,14 @@
+package org.ucl.gui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginHandlerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
